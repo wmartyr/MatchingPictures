0b6b7df4ffe03c219d6779fa1f273a2a92868810
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-12-16 09:13:22 +0800

    Initial State

--- MatchingPictures/ContentView.swift
+++ MatchingPictures/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animals = ["bear", "buffalo", "chick", "chicken", "cow", "crocodile", "dog", "duck", "elephant", "frog", "giraffe", "goat", "gorilla", "hippo", "horse", "monkey", "moose", "narwhal", "owl", "panda", "parrot", "penguin", "pig", "rabbit", "rhino", "sloth", "snake", "walrus", "whale", "zebra"]
+    @State private var numberOfAnimals = 0
+    @State private var gridSize = 4
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
