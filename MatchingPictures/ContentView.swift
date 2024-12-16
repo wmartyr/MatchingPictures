9fc1e2e95b21d58ef187153020313122bf08ae6b
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-12-16 08:43:33 +0800

    Initial Commit

---
+++ MatchingPictures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchingPictures
+//
+//  Created by Woodrow Martyr on 16/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchingPictures/MatchingPicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingPicturesApp.swift
+//  MatchingPictures
+//
+//  Created by Woodrow Martyr on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchingPicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
