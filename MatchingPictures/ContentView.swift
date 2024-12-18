74aca0ba7cee445c69aff32b3d3a06b6eea0f732
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-12-18 12:09:40 +0800

    Last update for now.

--- MatchingPictures/ContentView.swift
+++ MatchingPictures/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animals = ["bear", "buffalo", "chick", "chicken", "cow", "crocodile", "dog", "duck", "elephant", "frog", "giraffe", "goat", "gorilla", "hippo", "horse", "monkey", "moose", "narwhal", "owl", "panda", "parrot", "penguin", "pig", "rabbit", "rhino", "sloth", "snake", "walrus", "whale", "zebra"]
-    @State private var numberOfAnimals = 0
+    @State private var numberOfAnimals = 2
     @State private var gridSize = 4
     
     var body: some View {
@@ -19,7 +19,22 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
+        List {
+            ForEach(chooseAnimals(numberOfAnimals), id: \.self) { animal in
+                Text(animal)
+            }
+        }
         .padding()
+    }
+    
+    
+    func chooseAnimals(_ numberOfAnimals: Int) -> [String] {
+        var chosenAnimals = [String]()
+        for _ in 0..<numberOfAnimals {
+            let randomIndex = Int.random(in: 0..<self.animals.count)
+            chosenAnimals.append(animals[randomIndex])
+        }
+        return chosenAnimals
     }
 }
 
